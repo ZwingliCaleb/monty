45a406f13aae08748a8c4c3c171de3e555b5d75d
ZwingliCaleb <calebzwingli@gmail.com>
2023-01-19 17:36:25 -0800

    nop.c file added

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * nop - function to do nothing
+ * @stack: pointer to pointer to the double linked list
+ * @line_number: number of lines in file
+ * Return: nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	return;
+}
